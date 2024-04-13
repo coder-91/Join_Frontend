59cc4a4e1afbac44267de3518aa4c8d3ff282344
VeyselKaraali <veysel-karaali@outlook.de>
2024-04-13 04:49:58 +0200

    Added Imprint component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,22 @@ import { TaskViewComponent } from './components/task-view/task-view.component';
 import { PrivacyPolicyViewComponent } from './components/privacy-policy-view/privacy-policy-view.component';
 import { LegalNoticeViewComponent } from './components/legal-notice-view/legal-notice-view.component';
 import { HelpViewComponent } from './components/help-view/help-view.component';
+import { ImprintViewComponent } from './components/imprint-view/imprint-view.component';
+import { LoginViewComponent } from './components/login-view/login-view.component';
+import { RegisterViewComponent } from './components/register-view/register-view.component';
 
 export const routes: Routes = [
-  { path: '', component: SummaryViewComponent },
-  { path: 'summary', component: SummaryViewComponent },
-  { path: 'task', component: TaskViewComponent },
-  { path: 'board', component: BoardViewComponent },
-  { path: 'contacts', component: ContactViewComponent },
-  { path: 'privacy-policy', component: PrivacyPolicyViewComponent },
-  { path: 'legal-notice', component: LegalNoticeViewComponent },
-  { path: 'help', component: HelpViewComponent },
+  { path: '', component: LoginViewComponent, outlet: 'auth' },
+  { path: 'login', component: LoginViewComponent, outlet: 'auth' },
+  { path: 'register', component: RegisterViewComponent, outlet: 'auth' },
+  { path: 'summary', component: SummaryViewComponent, outlet: 'primary'  },
+  { path: 'task', component: TaskViewComponent, outlet: 'primary'   },
+  { path: 'board', component: BoardViewComponent, outlet: 'primary'   },
+  { path: 'contacts', component: ContactViewComponent, outlet: 'primary'   },
+  { path: 'privacy-policy', component: PrivacyPolicyViewComponent, outlet: 'primary'   },
+  { path: 'legal-notice', component: LegalNoticeViewComponent, outlet: 'primary'   },
+  { path: 'imprint', component: ImprintViewComponent, outlet: 'primary'   },
+  { path: 'help', component: HelpViewComponent, outlet: 'primary'   },
 ];
 
 
