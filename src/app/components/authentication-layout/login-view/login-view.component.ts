b0e86eda7916562eb0d4fa22ee4d645cbcefc283
VeyselKaraali <veysel-karaali@outlook.de>
2024-04-15 06:11:40 +0200

    WIP: Login Form

--- src/app/components/authentication-layout/login-view/login-view.component.ts
+++ src/app/components/authentication-layout/login-view/login-view.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import {FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login-view',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatInputModule , MatFormFieldModule , MatIconModule, MatCheckboxModule , MatButtonModule, ReactiveFormsModule],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.scss'
 })
 export class LoginViewComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+  rememberMeFormControl = new FormControl(false);
+  
+
+
+  public onSubmit(): void {
+    console.log('Submit');
+  }
 
 }
