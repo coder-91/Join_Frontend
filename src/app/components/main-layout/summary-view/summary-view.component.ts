0b4e62a1f5c6028c14c6b6a92edce00e0a23131a
VeyselKaraali <veysel-karaali@outlook.de>
2024-04-20 11:52:20 +0200

    Implemented summary view

--- src/app/components/main-layout/summary-view/summary-view.component.ts
+++ src/app/components/main-layout/summary-view/summary-view.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-summary-view',
   standalone: true,
-  imports: [],
+    imports: [
+        MatIcon
+    ],
   templateUrl: './summary-view.component.html',
   styleUrl: './summary-view.component.scss'
 })
