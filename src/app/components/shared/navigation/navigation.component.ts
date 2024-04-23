d0f9c2588e863ce0be7b5f71b679de9e3d279bb1
VeyselKaraali <veysel-karaali@outlook.de>
2024-04-23 07:49:45 +0200

    Changed file structure

---
+++ src/app/components/shared/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterOutlet, RouterLinkActive, RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+}
