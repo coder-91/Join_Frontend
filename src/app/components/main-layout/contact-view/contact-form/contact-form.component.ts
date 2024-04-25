7397d812dd8c8bd5faf604875a83399fa2372df0
VeyselKaraali <veysel-karaali@outlook.de>
2024-04-25 13:56:25 +0200

    Desktop Version of create and edit contact

--- src/app/components/main-layout/contact-view/contact-form/contact-form.component.ts
+++ src/app/components/main-layout/contact-view/contact-form/contact-form.component.ts
@@ -1,23 +1,28 @@
 import {Component, Inject} from '@angular/core';
 import {Contact} from "../../../../models/entity/contact";
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatButton} from "@angular/material/button";
 import {MatCheckbox} from "@angular/material/checkbox";
-import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
+import {MatError, MatLabel} from "@angular/material/form-field";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [
-    MatButton,
+    CommonModule,
+    MatButtonModule,
     MatCheckbox,
     MatError,
-    MatFormField,
-    MatInput,
+    MatFormFieldModule,
+    MatInputModule,
     MatLabel,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
@@ -28,8 +33,8 @@ export class ContactFormComponent {
   constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<ContactFormComponent>, @Inject(MAT_DIALOG_DATA) public contact: Contact) {
     this.contactForm = this.fb.group({
       id: this.contact?.id,
-      email: new FormControl('', [Validators.required, Validators.email]),
       name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       phoneNumber: new FormControl('', [Validators.required]),
     });
 
@@ -37,7 +42,6 @@ export class ContactFormComponent {
       this.contactForm.patchValue(this.contact);
     }
   }
-
 
   public onSubmit() {
     if (this.contact) {
