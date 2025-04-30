055adbb9ad9a6986d53a650a00c6d8f73f8352e0
coder-91 <134809294+coder-91@users.noreply.github.com>
2025-04-30 03:04:22 +0200

    Adjust grid layout based on auth state for medium screens

--- src/app/components/master-layout/master-layout.component.ts
+++ src/app/components/master-layout/master-layout.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {HeaderComponent} from "../shared/header/header.component";
 import {NavigationComponent} from "../shared/navigation/navigation.component";
 import {RouterOutlet} from "@angular/router";
 import {ScrollResetDirective} from "../../directives/scrollReset/scroll-reset.directive";
+import {AuthService} from "../../services/authService/auth.service";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-master-layout',
@@ -11,11 +13,12 @@ import {ScrollResetDirective} from "../../directives/scrollReset/scroll-reset.di
     HeaderComponent,
     NavigationComponent,
     RouterOutlet,
-    ScrollResetDirective
+    ScrollResetDirective,
+    AsyncPipe
   ],
   templateUrl: './master-layout.component.html',
   styleUrl: './master-layout.component.scss'
 })
 export class MasterLayoutComponent {
-
+  authService = inject(AuthService);
 }
